ee416918791fd0064eaa857c270a206fbb53ac46
Matt Ingold <mingold4@uncc.edu>
2019-03-25 20:34:11 +0000

    cleaning some db files

--- db/20190313222253_create_notes.rb
+++ db/20190313222253_create_notes.rb
@@ -1,7 +1,7 @@
 class CreateNotes < ActiveRecord::Migration[5.2]
   def change
     create_table :notes do |t|
-      t.integer :noteid
+      t.int :noteid
       t.string :notetitle
       t.string :username
       t.string :category
