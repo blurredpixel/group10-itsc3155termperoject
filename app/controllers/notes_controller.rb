3fa0453ef6865784784ad7746869e75a663e0f5f
David Fries <dfries4@gmail.com>
2019-04-29 11:34:22 -0400

    updated note creation redirect

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -39,7 +39,7 @@ class NotesController < ApplicationController
     respond_to do |format|
       if @note.save
         sendText
-        format.html { redirect_to @note, notice: 'Note was successfully created.' }
+        format.html { redirect_to pages_edit_sticky_path  }
         format.json { render :show, status: :created, location: @note }
       else
         format.html { render :new }
